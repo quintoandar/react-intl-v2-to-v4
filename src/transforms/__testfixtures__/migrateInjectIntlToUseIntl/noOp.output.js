0af414279bb794b2e5239285b74efa505b823e35
Daniela Yassuda <daniela.yassuda@quintoandar.com.br>
2020-06-16 13:37:31 -0300

    Return undefined when no-op

--- src/transforms/__testfixtures__/migrateInjectIntlToUseIntl/noOp.output.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-injectIntl();
--- src/transforms/migrateInjectIntlToUseIntl.ts
+++ src/transforms/migrateInjectIntlToUseIntl.ts
@@ -17,7 +17,7 @@ const transform: Transform = (file, api) => {
     });
 
   if (injectIntlImports.size() === 0) {
-    return root.toSource();
+    return undefined;
   }
 
   // Replace injectIntl import with useIntl
